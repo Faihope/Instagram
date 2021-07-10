126a6a2af7d08c32366230a884d6a5188963a42c
Fai <chepngetichfaith46@gmail.com>
2021-07-10 15:37:21 +0300

    removes users app,creates login and register file inside instagram app and creates url for them

--- insta/settings.py
+++ insta/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'instagram',
     'bootstrap3',
-    'users'
+    
 ]
 
 MIDDLEWARE = [
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.db.models.query import ValuesIterable
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^upload/$',views.upload,name = 'upload'),
+    path('register',views.register,name='register'),
+    path('login',views.login,name='register'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
