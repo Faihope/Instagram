2ab1b536655d2b03a788ee4c79135d87cb9cd666
Fai <chepngetichfaith46@gmail.com>
2021-07-10 13:52:16 +0300

    Add upload image file, create its view function and link it to the navbar

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image
 
 # Create your views here.
 
 def welcome(request):
-    return render (request,'base.html')
+    photos=Image.objects.all()
+    context= { 'photos':photos}
+    return render (request,'welcome.html',context)
+
+def upload(request):
+    if request.method == 'POST':
+
+        data=request.POST
+        image=request.FILES.get('image')
+        photo = Image.objects.create(
+            
+                image=image
+                )
+        return redirect('welcome')
+
+
+    return render(request,'upload.html')
+
