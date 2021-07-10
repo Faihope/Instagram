daea6f9a8b3cf99d160bebcfe2e0d8c3afede493
Fai <chepngetichfaith46@gmail.com>
2021-07-10 11:58:48 +0300

    Add profile and Image class to the models.py file

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_pic=models.ImageField(null=False,blank=True)
+    bio=models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.bio
+
+
+class Image(models.Model):
+    image=models.ImageField(blank=True,null=False)
+    name=models.CharField(max_length=100)
+    caption=models.TextField(max_length=400)
+    likes=models.IntegerField(default=0)
+    comments=models.CharField(max_length=1000)
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+   
+
+    def __str__(self):
+        return self.name
