e941d46d834bcab64857f079b304342d9c1e7b59
Fai <chepngetichfaith46@gmail.com>
2021-07-11 16:48:10 +0300

    Add test case to test the delete profile and creates delete profile method

--- instagram/tests.py
+++ instagram/tests.py
@@ -44,6 +44,12 @@ class ProfileTestclass(TestCase):
         self.myprofile.save_profile()
         profile=Profile.objects.all()
         self.assertTrue(len(profile)>0)
+
+    def test_delete_profile(self):
+        self.myprofile.save_profile()
+        profile_record=Profile.objects.all()
+        self.myprofile.delete_profile()
+        self.assertTrue(len(profile_record)==0)
     
 
 
